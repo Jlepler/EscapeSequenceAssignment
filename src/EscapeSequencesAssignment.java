eb78bd34670fa262b8dde22f334999bdc5f46777
jlepler <jlepler@montgomerycollege.edu>
2019-06-12 15:33:20 -0400

    Initial commit of EscapeSequenceAssignment

---
+++ src/EscapeSequencesAssignment.java
@@ -0,0 +1,21 @@
+public class EscapeSequencesAssignment {
+    public static void main(String [] args){
+
+        System.out.println("********\n*\t   *\n*\t   *\n*\t   *\n*\t   *\n********\n\n  ***  " + //in a single line using \t
+                "\n *   *" +    //using multiple lines using \n only
+                "\n*     *" +
+                "\n*     *" +
+                "\n *   *" +
+                "\n  ***" +
+                "\n" +
+                "\n" +
+                "\n   *" +
+                "\n  * *" +
+                "\n *   *" +
+                "\n*     *" +
+                "\n *   *" +
+                "\n  * *" +
+                "\n   *");
+
+    }
+}
